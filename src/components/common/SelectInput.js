4218c0cd55bbd2ae3cb287800061daf203d25c21
Rich Anderson <rich.anderson@turner.com>
2016-07-20 08:53:41 -0400

    courseForm add

---
+++ src/components/common/SelectInput.js
@@ -0,0 +1,31 @@
+import React, {PropTypes} from 'react';
+
+const SelectInput = ({name, label, onChange, placeholder, value, error, options}) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <div className="field">
+        <input
+          type="text"
+          name={name}
+          className="form-control"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}/>
+        {error && <div className="alert alert-danger">{error}</div>}
+      </div>
+    </div>
+  );
+};
+
+SelectInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string,
+  options: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default SelectInput;
